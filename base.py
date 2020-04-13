a42036f86f5745375f4dfa6122b011130261120a
Aaron Olkin <aaronolkin@gmail.com>
2020-04-13 00:08:44 -0400

    Fancier render

--- base.py
+++ base.py
@@ -3,19 +3,84 @@
 from math import pi, sin, cos
 from random import random, randint
 
+import sys
+
+import pdir
+
+#from direct.showbase.PythonUtil import *
 from panda3d.core import loadPrcFileData
 #loadPrcFileData("", "want-directtools #t")
 #loadPrcFileData("", "want-tk #t")
+loadPrcFileData('', 'win-size 1280 720') 
 
-from panda3d.core import Point3, Vec3
-from panda3d.core import TextNode, TextFont, AntialiasAttrib
-
+from panda3d.core import *
 from panda3d.bullet import *
 
 from direct.showbase.ShowBase import ShowBase
 from direct.task import Task
 from direct.actor.Actor import Actor
 from direct.interval.IntervalGlobal import Sequence
+
+from cube import makeCube
+
+if "help" in sys.argv:
+    print("""Command Line Arguments:
+    fps:\tdisplay FPS meter
+    debug:\tdisplay debug meter
+    bullet:\tdisplay bullet RBs
+    pause:\tstart with physics paused
+    """)
+    exit(0)
+
+def makeBoundaryBox(render, world):
+    boundaryNode = BulletRigidBodyNode("Boundary")
+    boundaryNode.setFriction(0)
+    boundaryNode.setRestitution(1)
+    boundaryNp = render.attachNewNode(boundaryNode)
+
+    vdata = GeomVertexData('', GeomVertexFormat.getV3(), Geom.UHStatic)
+    vdata.setNumRows(8)
+    vertices = GeomVertexWriter(vdata, "vertex")
+    for i in (-1, 1):
+        vertices.addData3(-1, -1, i)
+        vertices.addData3(-1, 1, i)
+        vertices.addData3(1, -1, i)
+        vertices.addData3(1, 1, i)
+    prim = GeomTriangles(Geom.UHStatic)
+    for a, b, c, d in (
+            (0, 1, 2, 3),
+            (0, 1, 4, 5),
+            (1, 3, 5, 7),
+            (3, 2, 7, 6),
+            (2, 0, 6, 4),
+            (4, 5, 6, 7)
+    ):
+        prim.addVertices(c, b, a)
+        prim.addVertices(b, c, d)
+    geom = Geom(vdata)
+    prim.closePrimitive()
+    geom.addPrimitive(prim)
+
+    for pos, shape in (
+            ((0, 0,  -18), Vec3(32, 24,  1)),
+            ((0, 0,   18), Vec3(32, 24,  1)),
+            ((0, -24,  0), Vec3(32,  1, 18)),
+            ((0,  24,  0), Vec3(32,  1, 18)),
+            ((-32, 0,  0), Vec3( 1, 24, 18)),
+            (( 32, 0,  0), Vec3( 1, 24, 18)),
+    ):
+        boundaryShape = BulletBoxShape(shape)
+        boundaryNode.addShape(boundaryShape,
+                              TransformState.makePos(pos))
+        if pos[1] >= 0:
+            node = GeomNode('square')
+            node.addGeom(geom)
+            np = render.attachNewNode(node)
+            np.setPos(pos)
+            np.setTwoSided(True)
+            np.setScale(shape)
+            np.setColor((1, 1, 1, 0.5))
+    world.attachRigidBody(boundaryNode)
 
 class TextApp(ShowBase):
 
@@ -24,6 +89,8 @@ class TextApp(ShowBase):
         #self.tutorialScene()
 
         self.setBackgroundColor(0, 0, 0)
+        self.setFrameRateMeter("fps" in sys.argv)
+        self.setSceneGraphAnalyzerMeter("debug" in sys.argv)
 
         debugNode = BulletDebugNode('Debug')
         debugNode.showWireframe(True)
@@ -31,7 +98,7 @@ class TextApp(ShowBase):
         debugNode.showBoundingBoxes(False)
         debugNode.showNormals(False)
         debugNP = self.render.attachNewNode(debugNode)
-        debugNP.show()
+        if "bullet" in sys.argv: debugNP.show()
 
         self.font = self.loader.loadFont(
             '/System/Library/Fonts/Supplemental/Arial Bold.ttf')
@@ -39,24 +106,75 @@ class TextApp(ShowBase):
         self.font.setPageSize(512, 512)
         self.font.setRenderMode(TextFont.RMSolid)
 
-        self.camera.setPos(0, -20, 10)
-        self.camera.setHpr(0, -20, 0)
+        self.camera.setPos(0, -90, 0)
+        #self.camera.setHpr(0, -20, 0)
         #self.taskMgr.add(self.spinCameraTask, "spinCameraTask")
         self.disableMouse()
 
+        self.rbNodes = []
+        self.paused = "pause" in sys.argv
+
         self.world = BulletWorld()
-        self.world.setGravity(Vec3(0, 0, -.1))
+        self.world.setGravity(Vec3(0, 10, 0))
         self.world.setDebugNode(debugNP.node())
         self.taskMgr.add(self.update, 'update')
 
-        self.camera.setPos(0, -20, 0)
-        self.disableMouse()
+        makeBoundaryBox(self.render, self.world)
+
+        light = PointLight("Light")
+        light.setColor((10, 0, 10, 1))
+        #light.setAttenuation((1, 0, 0.5))
+        lightNp = render.attachNewNode(light)
+        lightNp.setPos(0, -90, 0)
+        self.render.setLight(lightNp)
+        #light.setShadowCaster(True, 512, 512)
+
+        ambient = PointLight("Ambient")
+        ambient.setColor((0, .2, 0, 1))
+        #ambient.setAttenuation((1, 0, 0.5))
+        ambientNp = render.attachNewNode(ambient)
+        self.render.setLight(ambientNp)
+
+        self.render.setShaderAuto()
 
         for i in range(5):
-            self.addText("Sample %s" % i, random() * 5, random() * 5, 2)
+            self.addText("Sample %s" % i,
+                         random() * 5, -24, random() * 5)
 
         self.targets = []
         self.createTarget("A", -2, 5, -1)
+
+        self.accept('1', self.debugCamera)
+        self.accept('2', self.debugNodes)
+        self.accept('c', self.oobe)
+        self.accept('g', self.toggleGravity)
+        self.accept('p', self.pause)
+
+    def debugCamera(self):
+        self.enableMouse()
+        print(self.camera.getPos(), self.camera.getHpr())
+
+    def debugNodes(self):
+        print("\n".join(["{}\tGravity: {}\tLinear: {}\tAngular: {}".format(
+            i, i.getGravity(), i.getLinearVelocity(), i.getAngularVelocity())
+                         for i in self.world.getRigidBodies()]))
+
+    def pause(self):
+        self.paused = not self.paused
+
+    def toggleGravity(self):
+        if self.world.getGravity()[1] > 0:
+            self.world.setGravity((0, 0, 0))
+        elif self.world.getGravity()[1] == 0:
+            self.world.setGravity((0, -10, 0))
+        else:
+            self.world.setGravity((0, 10, 0))
+
+    def update(self, task):
+        dt = globalClock.getDt()
+        if not self.paused:
+            self.world.doPhysics(dt)
+        return task.cont
 
     def createTarget(self, name, x=0, y=0, z=0):
         shape = BulletSphereShape(0.5)
@@ -71,72 +189,38 @@ class TextApp(ShowBase):
         text = TextNode("text")
         text.setFont(self.font)
         text.setText(value)
-
         m = text.getTransform()
         m[1][1] = 0.1
         text.setTransform(m)
 
-        shape = BulletBoxShape(Vec3(0.5, 0.5, 0.5))
         node = BulletRigidBodyNode("Text")
-        node.setMass(1)
-        node.addShape(shape)
-        
         np = self.render.attachNewNode(node)
         np.setPos(x, y, z)
-
         tnp = np.attachNewNode(text)
         tnp.setColor(random(), random(), random(), 1)
         #tnp.setAntialias(AntialiasAttrib.MMultisample)
 
+        ul, lr = tnp.getTightBounds()
+        halfExtents = (lr - ul) / 2
+        tnp.setPos(-halfExtents - ul)
+        shape = BulletBoxShape(halfExtents)
+        node.addShape(shape)
+        node.setMass(1)
+        node.setLinearSleepThreshold(0)
+        node.setFriction(0)
+        node.setRestitution(1)
+        node.setAngularDamping(0.5)
+        node.setLinearDamping(0)
+
+        self.rbNodes.append(node)
+
         self.world.attachRigidBody(node)
 
-
-    def update(self, task):
-        dt = globalClock.getDt()
-        self.world.doPhysics(dt)
-        return task.cont
-
-    def tutorialScene(self):
-        # Load the environment model.
-        self.scene = self.loader.loadModel("models/environment")
-        # Reparent the model to render.
-        self.scene.reparentTo(self.render)
-        # Apply scale and position transforms on the model.
-        self.scene.setScale(0.25, 0.25, 0.25)
-        self.scene.setPos(-8, 42, 0)
-
-        # Load and transform the panda actor.
-        self.pandaActor = Actor("models/panda-model",
-                                {"walk": "models/panda-walk4"})
-        self.pandaActor.setScale(0.005, 0.005, 0.005)
-        self.pandaActor.reparentTo(self.render)
-        # Loop its animation.
-        self.pandaActor.loop("walk")
-
-        posInterval1 = self.pandaActor.posInterval(13,
-                                                   Point3(0, -10, 0),
-                                                   startPos=Point3(0, 10, 0))
-        posInterval2 = self.pandaActor.posInterval(13,
-                                                   Point3(0, 10, 0),
-                                                   startPos=Point3(0, -10, 0))
-        hprInterval1 = self.pandaActor.hprInterval(3,
-                                                   Point3(180, 0, 0),
-                                                   startHpr=Point3(0, 0, 0))
-        hprInterval2 = self.pandaActor.hprInterval(3,
-                                                   Point3(0, 0, 0),
-                                                   startHpr=Point3(180, 0, 0))
-
-        # Create and play the sequence that coordinates the intervals.
-        self.pandaPace = Sequence(posInterval1, hprInterval1,
-                                  posInterval2, hprInterval2,
-                                  name="pandaPace")
-        self.pandaPace.loop()
-
-    # Define a procedure to move the camera.
     def spinCameraTask(self, task):
-        angleDegrees = abs(task.time * 6.0 % 360 - 180) - 90
+        angleDegrees = abs(task.time * 6.0 % 180 - 90) - 45
         angleRadians = angleDegrees * (pi / 180.0)
-        self.camera.setPos(20 * sin(angleRadians), -20 * cos(angleRadians), 0)
+        self.camera.setPos(20 * sin(angleRadians),
+                           -20 * cos(angleRadians), 10)
         self.camera.setHpr(angleDegrees, 0, 0)
         return Task.cont
 
